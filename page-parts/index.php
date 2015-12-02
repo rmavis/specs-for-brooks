1c7e23f0ace9c62175b2ded6d4c53ea9a7da776d
Richard Mavis <rmavis@gmail.com>
2015-12-01 22:10:07 -0800

    Updated styles.

--- page-parts/index.php
+++ page-parts/index.php
@@ -23,7 +23,7 @@ $body['body'] = <<<HTML
 <h1 class="page-head">Brooks Cashbaugh</h1>
 
 <div class="text-block">
-  <p>Richard David James (born 18 August 1971), best known by his alias Aphex Twin, is an Irish-born British electronic musician and composer. He is also the co-founder of Rephlex Records with Grant Wilson-Claridge.[1] James has also released a number of EPs as AFX since 1991 including the Analogue Bubblebath and Analord series of EPs. James has also used and continues to use several other aliases, such as Polygon Window, Caustic Window, and the Tuss.</p>
+  <p>I've become a giant. I fill every street. I dwarf the rooftops, I hunchback the moon, stars dance at my feet.</p>
 </div>
 
 HTML;
